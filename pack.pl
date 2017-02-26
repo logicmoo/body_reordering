3190237db0342798771e0b4de28e72a0cdef58c1
root <logicmoo@gmail.com>
2017-02-26 10:25:59 -0800

    v0.0.8

---
+++ pack.pl
@@ -0,0 +1,9 @@
+name(body_reordering).
+version('0.0.8').
+title('Utils to optimize the order of literals in a bodies').
+keywords([source,goal_expansion]).
+author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+packager( 'TeamSPoon/LogicMoo', 'https://github.com/TeamSPoon/' ).
+maintainer( 'TeamSPoon', 'https://github.com/TeamSPoon/' ).
+home( 'https://github.com/TeamSPoon/body_reordering' ).
+download( 'https://github.com/TeamSPoon/body_reordering/release/*.zip' ).
