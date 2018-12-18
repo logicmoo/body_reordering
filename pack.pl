20f9d214d2b5ab825784d1190b598fecae15a0b0
Douglas R. Miles <logicmoo@gmail.com>
2018-12-18 16:59:17 +0000

    1.2.111

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(body_reordering).
-version('1.1.118').
+version('1.2.111').
 title('Clause expansion Utils for deciding which order to run Goals in a body').
 keywords([source,goal_expansion,term_expansion,database,utility]).
 
@@ -8,6 +8,5 @@ packager('TeamSPoon', 'https://github.com/TeamSPoon/').
 maintainer('TeamSPoon', 'https://github.com/TeamSPoon/').
 home('https://github.com/TeamSPoon/body_reordering.git').
 download( 'https://github.com/TeamSPoon/body_reordering/release/*.zip').
-requires(file_scope).
-requires(subclause_expansion).
+requires(logicmoo_utils).
 autoload(false).
